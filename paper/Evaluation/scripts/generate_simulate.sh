12e82b08600356d8475528734116ac7d398692cb
Li Song <lsong@igm3.(none)>
2014-05-19 11:01:21 -0400

    Clean up the scripts

--- paper/Evaluation/scripts/generate_simulate.sh
+++ paper/Evaluation/scripts/generate_simulate.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-for i in 25 #10 20 280
+for i in 10 20 280
 do
 	let cnt=$i*4700000/101
 	#echo $cnt 
 	mason illumina -i -n 101 -N $cnt -pi 0 -pd 0 -pmm 0.01 -pmmb 0.005 -pmme 0.03 -sq -hs 0 -hi 0 -o simulate/ecoli_reads_${i}_1.fq e_coli_k12.fa
 done
-exit 
+#exit 
 
 #cov: 35x, 70x, 140x
 for i in 35 70 140
